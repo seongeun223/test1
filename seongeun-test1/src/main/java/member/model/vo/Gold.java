8cbb894b0d1f19e9ec213c40cb863da253a5f573
seongeun223 <jklun080921@gmail.com>
2024-06-07 17:56:52 +0900

    Repository 이자율 메소드 MemberRepository 클래스 생성

--- seongeun-test1/src/main/java/member/model/vo/Gold.java
+++ seongeun-test1/src/main/java/member/model/vo/Gold.java
@@ -1,8 +1,19 @@
 package member.model.vo;
 
-public class Gold extends Member{
+public class Gold extends Member implements Buyable{
 
     public Gold(String name, String grade, int point) {
         super(name, grade, point);
+    }
+
+    @Override
+    public double calculateInterest() {
+        double point = (getPoint() * 0.05);
+        return point;
+    }
+
+    @Override
+    public int buy(int price) {
+        return 0;
     }
 }
--- seongeun-test1/src/main/java/member/model/vo/Silver.java
+++ seongeun-test1/src/main/java/member/model/vo/Silver.java
@@ -1,8 +1,19 @@
 package member.model.vo;
 
-public class Silver extends Member{
+public class Silver extends Member implements Buyable{
 
     public Silver(String name, String grade, int point) {
         super(name, grade, point);
+    }
+
+    @Override
+    public double calculateInterest() {
+        double point = (getPoint() * 0.02);
+        return point;
+    }
+
+    @Override
+    public int buy(int price) {
+        return 0;
     }
 }
