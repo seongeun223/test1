47cd3d77fa4d07991477d7cc1bce960c1fc19c34
seongeun223 <jklun080921@gmail.com>
2024-06-09 14:18:31 +0900

    MemberRepository insertMember 메소드 printData 메소드 OverMemberException 예외처리

--- seongeun-test1/src/main/java/member/controller/MemberRepository.java
+++ seongeun-test1/src/main/java/member/controller/MemberRepository.java
@@ -1,16 +1,29 @@
 package member.controller;
 
 import member.model.vo.Member;
+import member.run.Run;
+
+import java.util.ArrayList;
 
 public class MemberRepository {
-    MemberRepository[] MemberList = new MemberRepository[40];
+    private final ArrayList<Member> memberList = new ArrayList<>(10);
 
-    public void insertMember(Member m) {
+
+
+    public void insertMember(Member m) throws OverMemberException{
+        if(memberList.size() < 10) {
+            memberList.add(m);
+        } else {
+            throw new OverMemberException();
+        }
 
     }
 
 
     public void printData() {
+        for(Member member : memberList) {
+            member.calculateInterest();
+        }
 
     }
 }
--- seongeun-test1/src/main/java/member/controller/OverMemberException.java
+++ seongeun-test1/src/main/java/member/controller/OverMemberException.java
@@ -2,4 +2,7 @@ package member.controller;
 
 public class OverMemberException extends Exception{
 
+    public OverMemberException() {
+        super("Member List is full");
+    }
 }
